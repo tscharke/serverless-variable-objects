4c63c8ed8e795350c9f7244c6f08b1750fac1142
Thomas Scharke <thomas.scharke@gmail.com>
2019-04-24 18:04:50 +0200

    Raw and naive implementation for sls >= 1.41 only

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-'use strict';
+"use strict";
 
+const BbPromise = require("bluebird");
+const _ = require("lodash");
 
-module.exports = function convertVariables(S) {
+class ConvertEnvironmentVariables {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
 
-  let origGetEnvVars = S.classes.Runtime.prototype.getEnvVars;
+    this.commands = {
+      "variable-objects": {
+        usage: "Serverless-Plugin to turning objects into arrays",
+        lifecycleEvents: ["collect", "resolve", "apply"]
+      }
+    };
 
-  S.classes.Runtime.prototype.getEnvVars = (func, stage, region) =>
-    origGetEnvVars(func, stage, region)
-      .then(stringifyVars);
+    this.isOfflineHooked = false;
+    this.hooks = {
+      "before:offline:start:init": this.initOfflineHook.bind(this),
+      "before:offline:start": this.initOfflineHook.bind(this),
+      "before:invoke:local:invoke": this.initOfflineHook.bind(this),
+      "variable-objects:collect": this.collectEnvVars.bind(this),
+      "variable-objects:resolve": this.resolveEnvVars.bind(this),
+      "variable-objects:apply": this.applyEnvVars.bind(this)
+    };
 
-  function stringifyVars(variables) {
-    let keys = Object.getOwnPropertyNames(variables);
+    this.environmentVariables = {};
+  }
 
+  initOfflineHook() {
+    if (!this.isOfflineHooked) {
+      this.isOfflineHooked = true;
+      return this.serverless.pluginManager.run(["variable-objects"]);
+    }
+
+    return BbPromise.resolve();
+  }
+
+  collectEnvVars() {
+    return BbPromise.try(() => {
+      const envVars = {};
+
+      _.assign(envVars,this.serverless.service.provider.environment);
+
+      this.environmentVariables = envVars;
+
+      return BbPromise.resolve();
+    });
+  }
+
+  __convertVariables() {
+    const variables = {};
+    _.assign(variables, this.environmentVariables);
+
+    const keys = Object.getOwnPropertyNames(variables);
     keys.forEach(key => {
       let value = variables[key];
       if (typeof value == typeof {} || Array.isArray(value)) {
@@ -20,4 +62,23 @@ module.exports = function convertVariables(S) {
     });
     return variables;
   }
-};
+
+  resolveEnvVars() {
+    return BbPromise.try(() => {
+      return BbPromise.resolve(this.__convertVariables());
+    });
+  }
+
+  applyEnvVars() {
+    return BbPromise.try(() => {
+      if (this.isOfflineHooked) {
+        _.assign(
+          this.serverless.service.provider.environment,
+          this.__convertVariables()
+        );
+      }
+    });
+  }
+}
+
+module.exports = ConvertEnvironmentVariables;
